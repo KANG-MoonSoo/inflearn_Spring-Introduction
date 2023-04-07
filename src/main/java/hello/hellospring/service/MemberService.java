5a89be0953b3d806577a11c600f1956e96bd49bf
KANG-MoonSoo <munsu2483@gmail.com>
2023-04-07 18:51:22 +0900

    final

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -4,12 +4,14 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.thymeleaf.spring5.util.SpringVersionUtils;
 
 import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
 
@@ -20,8 +22,9 @@ public class MemberService {
     //회원가입
     public Long join(Member member){
         validateDuplicateMember(member);   //중복 회원 검증
-         memberRepository.save(member);
+        memberRepository.save(member);
         return member.getId();
+
     }
     private void validateDuplicateMember(Member member){
         memberRepository.findByName(member.getName())
